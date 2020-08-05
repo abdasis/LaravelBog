2c3028c28d75f7d1d0e949373a50e792ed5ad6a6
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2020-08-05 23:30:19 +0700

    update group belajar

--- resources/views/includes/left-sidebar.blade.php
+++ resources/views/includes/left-sidebar.blade.php
@@ -24,14 +24,33 @@
             <div class="card-text">
                 <a href="#">
                     <div class="row pl-2 middle-content">
-                        <img src="{{ asset('/assets/images/logo-dark.png') }}" alt="" class="img-fluid logo-group rounded-circle mr-2">
-                        <h3 class="nama-group">JavaScript ID</h3>
+                        <img src="{{ asset('/assets/images/ubuntu-id.jpg') }}" alt="" class="img-fluid logo-group rounded-circle mr-2">
+                        <h3 class="nama-group">Ubuntu Indonesia</h3>
                     </div>
                 </a>
                 <a href="https://web.facebook.com/groups/phpid" target="_blank">
                     <div class="row pl-2 middle-content">
                         <img src="{{ asset('/assets/images/php-id.jpg') }}" alt="" class="img-fluid logo-group rounded-circle mr-2">
                         <h3 class="nama-group">PHP Indonesia</h3>
+                    </div>
+                </a>
+                <a href="https://web.facebook.com/groups/phpid" target="_blank">
+                    <div class="row pl-2 middle-content">
+                        <img src="{{ asset('/assets/images/laravel-id.jpg') }}" alt="" class="img-fluid logo-group rounded-circle mr-2">
+                        <h3 class="nama-group">Laravel Indonesia</h3>
+                    </div>
+                </a>
+                <a href="https://web.facebook.com/groups/phpid" target="_blank">
+                    <div class="row pl-2 middle-content">
+                        <img src="{{ asset('/assets/images/react-id.jpg') }}" alt="" class="img-fluid logo-group rounded-circle mr-2">
+                        <h3 class="nama-group">ReactJs Indonesia</h3>
+                    </div>
+                </a>
+
+                <a href="https://web.facebook.com/groups/phpid" target="_blank">
+                    <div class="row pl-2 middle-content">
+                        <img src="{{ asset('/assets/images/vue-id.jpg') }}" alt="" class="img-fluid logo-group rounded-circle mr-2">
+                        <h3 class="nama-group">Vue Indonesia</h3>
                     </div>
                 </a>
             </div>
