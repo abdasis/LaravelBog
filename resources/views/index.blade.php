9d9ad5dadb0d43094252e90488cd0eff2d170723
Abd. Asis <id.abdasis@gmail.com>
2020-12-27 11:02:07 +0700

    ganti plugin sharethis

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -54,8 +54,9 @@ Saya Software Engineer berasal dari Indonesia, dengan bahasa pemrograman utama P
                 <div class="col-md-4">
                     <div class="card">
                         @if ($post->thumbnail_artikel  && file_exists(public_path('thumbnail-artikel') . '/' . $post->thumbnail_artikel))
-                        <img class="card-img-top img-post-thumbnail-small" src="{{ asset('thumbnail-artikel/') .'/'.$post->thumbnail_artikel }}" alt="Card image cap">
-                        @else
+<a href="{{ asset('thumbnail-artikel/') .'/'.$post->thumbnail_artikel }}" class="progressive replace card-img-top img-post-thumbnail">
+                        <img src="{{ asset('thumbnail-artikel/') .'/'.$post->thumbnail_artikel }}" class="preview card-img-top img-post-thumbnail" alt="image" />
+                    </a>                        @else
                         <img class="card-img-top img-post-thumbnail-small" src="{{ asset('assets/images/patient_forms_drib.jpg') }}" alt="Card image cap">
                         @endif
                         <div class="card-body">
